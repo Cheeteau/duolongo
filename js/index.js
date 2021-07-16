4d05c74df2508d88bce48d8e99ed9e52f64f9ef9
Co_Zax <sushibar2929@gmail.com>
2021-07-16 02:41:20 +0200

    JS advancements

--- js/index.js
+++ js/index.js
@@ -2,15 +2,36 @@
 const langsList = document.querySelector("#_langsList");
 const langMd = document.querySelector("#_langMarkdown");
 
+const markdownContents = {};
+const available = [
+    "🇩🇰.danois.md"
+];
+
 /* Functions */
+function saveMarkdownData(){ window.localStorage.setItem("markdowns", markdownContents); }
+function retrieveMarkdownData(){ return window.localStorage.getItem("markdowns"); }
+
 function getMarkdowns(){
+    return new Promise(async (res, rej) => {
+        for (const markdown in available){
+            const request = await fetch(`/markdown/${markdown}`);
+            const name = markdown.split(".")[1];
+    
+            if (!retrieveMarkdownData()[name]) markdownContents[name] = request;
+        }
+    
+        saveMarkdownData();
+        return res();
+    });
 }
 
-function displayLanguages(){
-
+async function displayLanguages(){
+    const get = await getMarkdowns();
+    console.log(markdownContents);
 }
 
 /* Main async function */
 (async () => {
-
+    if (markdownContents == null) saveMarkdownData();
+    displayLanguages();
 })();
