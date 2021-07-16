f9caa711a6f045dac9d7efbd1243c859c55d7731
Co_Zax <sushibar2929@gmail.com>
2021-07-16 03:09:33 +0200

    JS advancements

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const markdownContents = {};
 const available = [
     {
         name: "danois",
-        emote: "🇩🇰"
+        emoji: "🇩🇰"
     }
 ];
 
@@ -23,9 +23,22 @@ function getMarkdowns(){
     });
 }
 
+function getTwitterFlagFromEmoji(emoji){
+    const emojiFirst = emoji.codePointAt(0).toString(16);
+    const emojiSecond = emoji.codePointAt(2).toString(16);
+    return `https://abs-0.twimg.com/emoji/v2/svg/${emojiFirst}-${emojiSecond}.svg`;
+}
+
 async function displayLanguages(){
     await getMarkdowns();
-    console.log(markdownContents);
+    
+    for (const lang of available){
+        const li = document.createElement("li");
+        const langName = lang.name.charAt(0).toUpperCase() + lang.name.substr(0, 1);
+
+        li.innerHTML = `<img src="${getTwitterFlagFromEmoji(lang.emoji)}"> ${langName}`;
+        langsList.append(li);
+    }
 }
 
 /* Main async function */
