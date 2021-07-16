7252ce04fc7f12a7faf5b85e6c95a0b3d5cb1999
Co_Zax <sushibar2929@gmail.com>
2021-07-16 02:08:30 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+/* Variables */
+const langsList = document.querySelector("#_langsList");
+const langMd = document.querySelector("#_langMarkdown");
+
+/* Functions */
+function getMarkdowns(){
+}
+
+function displayLanguages(){
+
+}
+
+/* Main async function */
+(async () => {
+
+})();
