047edc06f6b9f0c3d7548fb9fc63d52224c5462a
Co_Zax <sushibar2929@gmail.com>
2021-07-27 01:55:23 +0200

    JS Searchbar

--- js/index.js
+++ js/index.js
@@ -109,11 +109,10 @@ function displayMarkdown(lang){
     // Quand l'utilisateur tappe un mot
     inputSearch.addEventListener("keyup", () => {
         const value = inputSearch.value.replace(" ", "");
-        if(value.length == 0){
-            document.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"));
-            return langMd.scroll(0, 0);
-        }
+        if(value.length == 0) return langMd.scroll(0, 0);
 
+        document.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"));
+        
         // Récupère un élément avec ce mot
         const elements = [...document.querySelectorAll("td")].filter(e => e.innerHTML.includes(value));
         const element = elements[0];
@@ -123,7 +122,6 @@ function displayMarkdown(lang){
             element.scrollIntoView();
             element.parentNode.classList.add("selected");
         }else{
-            document.querySelectorAll(".selected").forEach(e => e.classList.remove("selected"));
             return langMd.scroll(0, 0);
         }
     });
